0876de74b6d1f364511904c175fc60d46c1212ca
Lewis Halstead <lewishalstead5@gmail.com>
2024-10-18 21:00:17 +0100

    psql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+dotenv.load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+if DB_PASSWORD is None:
+    print("DB_PASSWORD not found")
+
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://admin:admin@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = f'postgresql://fastapi-todo-prod-db_owner:{DB_PASSWORD}@ep-square-band-a2xo5m8j.eu-central-1.aws.neon.tech/fastapi-todo-prod-db?sslmode=require'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # FOR SQLITE: connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
