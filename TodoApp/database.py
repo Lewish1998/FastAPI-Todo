85806bd73c69cea3c3f9640ce4c12ea0609c0ccd
Lewis Halstead <108507629+Lewish1998@users.noreply.github.com>
2024-10-24 21:30:23 +0100

    remove local postgres

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -11,7 +11,6 @@ if DB_PASSWORD is None:
     print("DB_PASSWORD not found")
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# SQLALCHEMY_DATABASE_URL = 'postgresql://admin:admin@localhost/TodoApplicationDatabase'
 SQLALCHEMY_DATABASE_URL = f'postgresql://fastapi-todo-prod-db_owner:{DB_PASSWORD}@ep-square-band-a2xo5m8j.eu-central-1.aws.neon.tech/fastapi-todo-prod-db?sslmode=require'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL) # FOR SQLITE: connect_args={'check_same_thread': False})
